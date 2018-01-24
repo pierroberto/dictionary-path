d83e569a1018fdf3d49c918e6c7b2e94720fa800
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-01-24 19:21:01 +0100

    Refactoring: remove console log and unused code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dictionary = (words, begin, end) => {
-  counter = 1;
+  let direction = 0;
+  let counter = 1;
   // I convert the begin and the end in numbers
   const beginNumber = begin.split("").map(letter => {
     return letter.charCodeAt(0);
@@ -10,7 +11,6 @@ const dictionary = (words, begin, end) => {
   });
   // It finds the first letter and remove from the array
   // the element from which it begun
-  const firstLetter = begin.charCodeAt(0);
   const index = words.indexOf(begin);
   words.splice(index, 1);
 
@@ -22,33 +22,23 @@ const dictionary = (words, begin, end) => {
   });
   list.push(beginNumber);
   direction = beginNumber[0] - endNumber[0];
-  return findPath(list, beginNumber, endNumber, direction);
+  return findPath(list, beginNumber, endNumber, direction, counter);
 };
 
-let direction = 0;
-let counter = 1;
-
-const findPath = (list, begin, end, direction) => {
-  if (!list.length) return counter;
+const findPath = (list, begin, end, direction, counter) => {
   direction = begin[0] - end[0];
-  console.log("LIST begin", list, "dir", direction);
   let diff = 0;
 
   for (let i = 0; i < list.length; i++) {
-    if (!list.length) return counter;
     for (let j = 0; j < list[i].length; j++) {
-      console.log("SECOND DEBUGS", i, j);
-      if (!list.length) return counter;
       if (list[i][j] !== begin[j]) {
         if (direction > 0) {
-          console.log("positive direction ...");
           if (list[i][0] <= begin[0]) {
             diff++;
           } else {
             list.splice(i, 1);
             diff = -1;
             i--;
-            if (!list.length) return counter;
           }
         } else if (direction < 0) {
           if (list[i][0] >= begin[0]) {
@@ -57,37 +47,22 @@ const findPath = (list, begin, end, direction) => {
             list.splice(i, 1);
             diff = -1;
             i--;
-            console.log("====== debugger =====");
-            if (!list.length) return counter;
           }
         }
       }
     }
-    console.log("CHECKING DIFFERENCES", diff);
-    if (diff === 0) {
-      list.splice(i, 1);
-      if (!list.length) return counter;
-    }
+    if (diff === 0) list.splice(i, 1);
     if (diff === 1) {
       counter++;
-      console.log("counter", counter);
       diff = 0;
       begin = list[i];
       list.splice(i, 1);
-      if (!list.length) return counter;
-
-      console.log("LIST final", list);
     }
-
     diff = 0;
   }
-  console.log("--------------- LOOP END ----------------------");
   if (list.length) {
-    k = 0;
-    return findPath(list, begin, end, direction);
+    return findPath(list, begin, end, direction, counter);
   }
-
-  //return list, begin, end;
   return counter;
 };
 
--- test/test.js
+++ test/test.js
@@ -2,6 +2,10 @@ const assert = require("assert");
 const dictionary = require("..");
 
 describe("Example", () => {
+  it("should return the correct result for the given example", () => {
+    const words = ["hit", "dot", "dog", "cog", "hot", "log"];
+    assert.equal(dictionary.dictionary(words, "hit", "cog"), 4);
+  });
   it("should work with other examples", () => {
     const words = ["a", "b", "c", "d"];
     assert.equal(dictionary.dictionary(words, "a", "d"), 3);
@@ -10,15 +14,11 @@ describe("Example", () => {
     const words = ["a", "b", "c", "d"];
     assert.equal(dictionary.dictionary(words, "d", "a"), 3);
   });
-  it("should return the correct result for the given example", () => {
-    const words = ["hit", "dot", "dog", "cog", "hot", "log"];
-    assert.equal(dictionary.dictionary(words, "hit", "cog"), 4);
-  });
-  it("should return the correct result for the given example", () => {
+  it("should work with other examples", () => {
     const words = ["hit", "hot", "hat"];
     assert.equal(dictionary.dictionary(words, "hat", "hot"), 2);
   });
-  it("should return the correct result for the given example", () => {
+  it("should work with other examples", () => {
     const words = ["it", "at", "or", "ot"];
     assert.equal(dictionary.dictionary(words, "at", "or"), 3);
   });
